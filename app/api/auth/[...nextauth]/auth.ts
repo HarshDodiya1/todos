f774bfb75c5b24df721a05d445f3895378892aa2
HarshDodiya1 <dodiyaharsh999@gmail.com>
2025-02-19 20:43:51 +0530

    Add dotenv for environment variable management and update auth configuration

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -2,24 +2,36 @@ import { AuthOptions, DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 declare module "next-auth" {
-    interface Session {
-      user: {
-        id: string;
-      } & DefaultSession["user"]
-    }
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
   }
-  
+}
+
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          access_type: "offline",
+          prompt: "consent",
+          response_type: "code",
+        },
+      },
     }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
   session: {
     strategy: "jwt",
     maxAge: 365 * 24 * 60 * 60, // 1 year
@@ -32,4 +44,4 @@ export const authOptions: AuthOptions = {
       return session;
     },
   },
-}; 
+};
